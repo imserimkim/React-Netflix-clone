478143b9be9fc1c881d94cf996a7e75d6c2ae738
kimserim <112158792+imserimkim@users.noreply.github.com>
2023-02-06 12:16:44 +0900

    배포

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename='React-Netflix-clone-2'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
