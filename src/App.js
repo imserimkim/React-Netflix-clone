514e04a25b8f4ebb0ff4041284ff10505e8e2856
kimserim <112158792+imserimkim@users.noreply.github.com>
2023-02-06 11:52:39 +0900

    2/6

--- src/App.js
+++ src/App.js
@@ -1,76 +1,70 @@
-import './App.scss';
-import Nav from './components/Nav'; 
-import Banner from './components/Banner';
-import requests from './api/requests';
-import Row from './components/Row';
-import Footer from './components/Footer';
-
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <Banner />
-
-      <Row 
-        title="오직 넷플릭스에서"
-        id="NO"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-
-      <Row title="지금 뜨는 콘텐츠" id="TN" fetchUrl={requests.fetchTrending} />
-      <Row title="오늘 대한민국의 TOP 10" id="TR" fetchUrl={requests.fetchTopRated} />
-      <Row title="블록버스터 영화" id="AM" fetchUrl={requests.fetchActionMovies} />
-      <Row title="코미디 영화" id="CM" fetchUrl={requests.fetchComedyMovies} />
-
-      <Footer />
-      console.log(Banner);
-    </div>
-  );
-  }
-
-export default App;
-
-
-
-// import axios from "axios";
-// import { useState, useEffect } from "react";
+// import './App.scss';
+// import Nav from './components/Nav'; 
+// import Banner from './components/Banner';
+// import requests from './api/requests';
+// import Row from './components/Row';
+// import Footer from './components/Footer';
 
 // function App() {
-//   const KEY = "dcb877c29782034e77c94aac785199a2"
-//   const DATE = "20230121"
-  
-
-//   const [loading,setLoading] = useState(true);
-//   const [movies,setMovies] = useState([])
-//   const getMovies = async() => {
-//     const json = await (
-//        await fetch(
-//       `http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${KEY}&targetDt=${DATE}`
-//     )
-//     ).json();
-//     setMovies(json.boxOfficeResult.dailyBoxOfficeList);
-//     console.log(json.boxOfficeResult.dailyBoxOfficeList);
-//     setLoading(false);
-//   }
-//   useEffect(() => {
-//    getMovies();
-//   }, [])
 //   return (
-//     <div>
-//       {
-//         loading ? <h1>Loading…</h1> :
-//         <div>
-//             {movies.map(movie => 
-//             <div key={movie.movieCd}>       
-//               <h3>{movie.rank}위</h3>
-//               <p>{movie.movieNm}</p>
-//             </div>)} 
-//         </div>
-//       }
+//     <div className="App">
+//       <Nav />
+//       <Banner />
+
+//       <Row 
+//         title="오직 넷플릭스에서"
+//         id="NO"
+//         fetchUrl={requests.fetchNetflixOriginals}
+//         isLargeRow
+//       />
+
+//       <Row title="지금 뜨는 콘텐츠" id="TN" fetchUrl={requests.fetchTrending} />
+//       <Row title="오늘 대한민국의 TOP 10" id="TR" fetchUrl={requests.fetchTopRated} />
+//       <Row title="블록버스터 영화" id="AM" fetchUrl={requests.fetchActionMovies} />
+//       <Row title="코미디 영화" id="CM" fetchUrl={requests.fetchComedyMovies} />
+
+//       <Footer />
+//       console.log(Banner);
 //     </div>
 //   );
-// }
+//   }
 
 // export default App;
 
+
+
+import { Outlet, Routes, Route } from "react-router-dom";
+import "./App.scss";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import DetailPage from "./pages/DetailPage";
+import MainPage from "./pages/MainPage";
+import SearchPage from "./pages/SearchPage";
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="app">
+      <Routes> 
+        <Route path="/" element={<Layout />} >
+          <Route index element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Nav.scss';
+import { useNavigate } from "react-router-dom";
 import {useEffect, useState } from 'react'; 
 
 export default function Nav() {
     const [show, setshow] = useState(false); 
+    const [searchValue, setSearchValue] = useState("");
+    const navigate = useNavigate();
 
     useEffect(() => {      //리스너 등록 
         window.addEventListener("scroll",()=>{
@@ -22,14 +25,29 @@ export default function Nav() {
         };
     }, [/*third*/]);
 
+    const handleChange = (e) => {
+        setSearchValue(e.target.value);
+        navigate(`/search?q=${e.target.value}`);
+      };
+
     return (
         <nav className={`nav ${show && "nav_black"}`}>
             <img
                 alt='Netflix logo' 
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Netflix_2014_logo.svg/300px-Netflix_2014_logo.svg.png?20140810085105"
                 className='nav_logo'
-                onClick={() => window.location.reload()}
+                onClick={() => window.location.href = "/"}
             />
+
+                
+            <input
+                value={searchValue}
+                onChange={handleChange}
+                className="nav__input"
+                type="text"
+                placeholder="영화를 검색해주세요."
+            />
+
             <img 
                 alt="User logged"
                 src="http://occ-0-3077-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABTtt4ubPy0MpDz_mLS9xjaHT7a8NLpcJNvPDzEo-LZoTvmwsWCni05Eo_zlCzkJmc1ORSBcISNDKQbEQE7RDl6pnkmVOrrs.png?r=214"
